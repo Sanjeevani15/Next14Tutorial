20dcfbbe6381b7cbc46dadaddb3ee53d83cb0dff
Sanjeevani-octet <66158817+Sanjeevani15@users.noreply.github.com>
2024-08-31 23:59:55 +0530

    fix: usage of params in the dynamic routes, also addition of typescript

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+type Params = {
+  productId: string;
+};
 
-const ProductDetails = () => {
-  return <div>ProductDetails - dynamic routes</div>;
+const ProductDetails = ({ params }: { params: Params }) => {
+  // for specifying type you can write  :  `{params }: { params: { productId: string;} }`
+  return (
+    <div>
+      <h1>ProductDetails - dynamic routes</h1>
+      <p>Current product id is : {params.productId}</p>
+    </div>
+  );
 };
 
 export default ProductDetails;
