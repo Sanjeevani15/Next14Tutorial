a773f253566c52126469e0e8fc3a58132f31209d
Sanjeevani-octet <66158817+Sanjeevani15@users.noreply.github.com>
2024-09-01 06:28:12 +0530

    learn: static meta data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "Generated for meta data",
+};
 
 const About = () => {
   return <div>This is about page</div>;
